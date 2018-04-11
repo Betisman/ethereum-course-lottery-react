6ba8826fd319a37e9e685a68c58d1f5f4737c0b1
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-04-12 00:42:00 +0200

    using the lottery contract from the web page

--- src/lottery.js
+++ src/lottery.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 
 // address and abi got by running "node deploy.js" in the lottery project (https://github.com/Betisman/ethereum-course-lottery/blob/master/deploy.js)
-const address = 'x07ae3b3cD26C83eB4b6577F3e4F1641F033714e6';
+const address = '0x07ae3b3cD26C83eB4b6577F3e4F1641F033714e6';
 const abi = [{ "constant": true, "inputs": [], "name": "manager", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "pickWinner", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getPlayers", "outputs": [{ "name": "", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "enter", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "players", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }];
 
 export default new web3.eth.Contract(abi, address);
